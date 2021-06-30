1c4dd39c47248983c83847ac106f962788387bed
Pete Bingel <pete.bingel@blackbaud.me>
2021-06-30 00:09:10 -0400

    Added metatags

--- components/elements/Layout/Layout.tsx
+++ components/elements/Layout/Layout.tsx
@@ -8,12 +8,31 @@ export default function Layout({
   children,
   title = "Charleston Pride",
   description = "Charleston Pride",
+  imageUrl = "https://res.cloudinary.com/charlestonpride-org/image/upload/c_lpad,g_center,h_628,w_1200/v1625023527/logo_qxgcvs.jpg",
 }) {
   return (
     <>
       <Head>
         <title key="title">{title}</title>
         <meta key="description" name="description" content={description} />
+
+        {/* <!-- Primary Meta Tags --> */}
+        <meta name="description" content={description} />
+
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://charlestonpride.org" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={imageUrl} />
+
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://charlestonpride.org" />
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={description} />
+        <meta property="twitter:image" content={imageUrl} />
+
         <link rel="icon" href="/favicon.ico" />
         <link
           href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700"
