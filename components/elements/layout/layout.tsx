9c6100798fbbb1a7a4b8224dcfad58098098837d
Pete Bingel <bingelp@gmail.com>
2022-08-08 09:22:48 -0400

    Vendor (#13)
    * vendor registration 22
    
    * updated times
    
    Co-authored-by: Pete Bingel <pete.bingel@blackbaud.me>

--- components/elements/layout/layout.tsx
+++ components/elements/layout/layout.tsx
@@ -54,14 +54,6 @@ export default function Layout({
           href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700"
           rel="stylesheet"
         />
-        {/* Constant Contact */}
-        <script> var _ctct_m = "fc55b4bacaf81ea6e70101a81f49e6ae"; </script>
-        <script
-          id="signupScript"
-          src="//static.ctctcdn.com/js/signup-form-widget/current/signup-form-widget.min.js"
-          async
-          defer
-        ></script>
       </Head>
       <header>
         <MainNavBar />
--- pages/vendor-registration.tsx
+++ pages/vendor-registration.tsx
@@ -1,48 +1,179 @@
 import React from "react";
 import Layout from "../components/elements/layout/layout";
-import { Container } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
+import CloudImage from "../components/elements/cloudImage/cloudImage";
+import ExternalFrame from "../components/modules/externalFrame/externalFrame";
 
 const VendorRegistration = () => {
   return (
     <Layout
-      title="Charleston Pride Festival 2021 Vendor Registration"
-      description="Vendor registration for the Charleston Pride Festival 2021."
+      title="Charleston Pride Festival 2022 Vendor Registration"
+      description="Vendor registration for the Charleston Pride Festival 2022."
     >
-      <Container className="my-5">
-        <div className="card card-background card-background-mask-primary mt-5">
-          <div className="full-background"></div>
-          <div className="card-body pt-5 text-center">
-            <h1 className="text-white mb-2">2021 Pride Festival Update</h1>
+      <ExternalFrame
+        title="Charleston Pride Festival 2022 Vendor Registration"
+        color="info"
+        iframeSrc="https://registration.planningpod.com?ssid=NDE0ODQ0"
+        iframeHeight="2000px"
+      >
+        <Row>
+          <Col xs="10" lg="6" className="mx-auto py-5 text-lg-left text-center">
             <p className="lead">
-              It is with heavy hearts that <strong>Charleston Pride</strong> has
-              made the decision to postpone our annual Pride Parade as well as
-              the October 3rd Pride Festival at Riverfront Park. Originally, the
-              festival was moved to North Charleston to accommodate distance and
-              safety, but the rising Covid cases disallow holding a responsible
-              event for participants.
+              The Charleston Pride Festival will be held at{" "}
+              <strong>Riverfront Park</strong> in North Charleston on{" "}
+              <strong>Sunday, November 13, 2022</strong>. We are excited to
+              bring the festival back after last year’s cancellation, and invite
+              you to join Charleston’s largest Pride event of the year.
             </p>
             <p className="lead">
-              Several festivals in October have been postponed in the past week
-              out of health and safety concerns, both in South Carolina and
-              surrounding states. <strong>Charleston Pride</strong> agrees that
-              this is the best course of action to ensure the well-being of
-              everyone.
+              Being a vendor is a great way to promote your organization and/or
+              company’s commitment to equal rights for all Charlestonians!
+              Additionally, a booth at the Pride Festival will promote your
+              organization and/or company among a deeply diverse group of
+              individuals. In 2019, during our 10th Anniversary, we had an
+              estimated 7,500+ attendants! These attendees were not only gay,
+              lesbian, bisexual, or transgender, but also allied individuals who
+              appreciate the values of equality. By being at the Charleston
+              Pride Festival, you can increase your organization and/or
+              company’s visibility among much of Charleston’s residents.
             </p>
-            <p className="lead">
-              For the duration of the pandemic, we plan to execute smaller,
-              outdoor events and indoor-optional, for those comfortable. To
-              maintain both a safe and fun environment, we will implement strict
-              health-related protocols. Please understand that we did not make
-              this decision lightly and we look forward to gathering again as
-              soon as it is safer.
-            </p>
-            <p className="lead">
-              With love and pride,
-              <br /> Charleston Pride Board of Directors
+          </Col>
+          <Col
+            xs={{ span: 8, offset: 2 }}
+            lg="4"
+            className="mx-lg-0 mx-auto px-lg-0 px-md-0 my-auto"
+          >
+            <CloudImage
+              className="w-100 border-radius-lg shadow"
+              imageId="2022/lvplxl"
+            ></CloudImage>
+          </Col>
+        </Row>
+
+        <div className="card my-3">
+          <div className="card-body">
+            <h4 className="card-title">Vendor Fees:</h4>
+            <ul className="list-group">
+              <li className="list-group-item">
+                <strong>10x10 Vendor Plot:</strong> Fees for a 10x10 vendor plot
+                is $300. Non-profits can apply at the reduced rate of $150.
+              </li>
+              <li className="list-group-item">
+                <strong>10x10 Premium Vendor Plot:</strong> Fees for a Main
+                Stage Area Premium 10x10 vendor plot is $500.
+              </li>
+              <li className="list-group-item">
+                <strong>20x20 Vendor Plot:</strong> A limited number of 20x20
+                plots will also be available for larger tents for $750.
+                Non-profits can apply at the reduced rate of $375.
+              </li>
+              <li className="list-group-item">
+                If electricity is needed, a $75 fee will be added to the
+                registration [$150 for 20x20 tents]. Electricity availability is
+                limited, and we reserve the right to cap the number of power
+                slots.
+              </li>
+              <li className="list-group-item">
+                <strong>Note:</strong> Charleston Pride only provides the plot
+                for a vendor space and does not supply tents, tables or chairs.
+                Payments are non-refundable in case of weather cancellation.
+                Your registration will roll over to the 2023 or future Festival.
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="card my-3">
+          <div className="card-body">
+            <h4 className="card-title">Food Trucks:</h4>
+            <p className="card-description">
+              Due to limited capacity and to ensure a variety of cuisine, food
+              trucks must be approved first. Apply below. After your acceptance
+              you will receive instructions on how to pay for your spot.
             </p>
           </div>
         </div>
-      </Container>
+
+        <div className="card my-3">
+          <div className="card-body">
+            <h4 className="card-title">Vendor Guidelines:</h4>
+            <p className="card-description">
+              Riverfront Park is a different venue from the past several
+              festivals, and certain rules have changed, so please read
+              carefully. You will get more specific information closer to the
+              festival, but below are some initial guidelines.
+            </p>
+            <ul className="list-group">
+              <li className="list-group-item">
+                Load-in will be on the day of the festival, and will begin at
+                8am. Tentative festival times are from 11AM to 5PM. Load-out
+                will begin after 5:15pm.{" "}
+              </li>
+              <li className="list-group-item">
+                No cars are allowed in the park due to the irrigation system, so
+                plan accordingly. Carts and wagons are acceptable.
+              </li>
+              <li className="list-group-item">
+                Tents are to be 10x10 ft [or 20x20 ] with straight legs, not
+                angled. No spikes are allowed at the park [irrigation system] so
+                tents must be bucketed, ie. legs in 5 gallon buckets weighted
+                down.{" "}
+              </li>
+              <li className="list-group-item">
+                There will be a separate area for parking, but limited to 1 per
+                vendor.{" "}
+              </li>
+              <li className="list-group-item">
+                Vendors will be allowed 10-15 minutes to unload. This does not
+                include your setup time, there should be ample time to erect
+                tents and setup displays etc.
+              </li>
+              <li className="list-group-item">
+                If selling a product, the South Carolina Department of Revenue
+                requires your Retail License to be displayed at all times.
+              </li>
+              <li className="list-group-item">
+                Charleston Pride Festival, Inc. requires you to be insured for
+                at least $1 million to cover any liability. If you do not have
+                coverage, you take responsibility for covering any claims.{" "}
+              </li>
+              <li className="list-group-item">
+                Charleston Pride Festival, Inc., nor the City of North
+                Charleston, takes any responsibility or liability for any damage
+                to your product, person, or equipment.
+              </li>
+              <li className="list-group-item">
+                All documentation, ie. proof of non-profit status, insurance
+                information, etc. must be on file with Charleston Pride Festival
+                by September 15. Information can be uploaded during or after
+                registration.
+              </li>
+              <li className="list-group-item">
+                Sale of alcohol is not permitted and is the sole right of
+                Charleston Pride Festival Inc.{" "}
+              </li>
+              <li className="list-group-item">
+                Vendors are responsible for cleaning up their area, disposing
+                and hauling off any trash.
+              </li>
+              <li className="list-group-item">
+                Vendor packets will be sent to the email address on-file two
+                weeks prior to the Festival.
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <p className="lead text-center my-3">
+          We are excited for a great 2022 Pride Festival and look forward to
+          your participation! If you have any questions, please feel free to
+          contact us at{" "}
+          <a href="mailto:festival@charlestonpride.org">
+            festival@charlestonpride.org
+          </a>
+          .
+        </p>
+      </ExternalFrame>
     </Layout>
   );
 };
