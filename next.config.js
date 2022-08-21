ccc967590fc89f013612c45bec496de0ad6d94e8
Pete Bingel <pete.bingel@blackbaud.me>
2022-08-21 11:39:13 -0400

    Fix config

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = {
       "/bylaws": { page: "/bylaws" },
       "/donate": { page: "/donate" },
       // "/friends-of-pride": { page: "/friends-of-pride" },
-      "/mailing-list": { page: "/mailing-list" },
+      "/tacos-and-tattas": { page: "/tacos-and-tattas" },
       "/our-team": { page: "/our-team" },
       "/serve-on-the-board": { page: "/serve-on-the-board" },
       "/store": { page: "/store" },
