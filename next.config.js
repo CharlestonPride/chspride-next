de064ce70f69894e3d48a165ad3a55934a1f9da0
Pete Bingel <peter.bingel@charlestonpride.org>
2024-06-06 21:51:09 -0400

    Remove delete routes

--- next.config.js
+++ next.config.js
@@ -5,18 +5,12 @@ module.exports = {
       "/": { page: "/" },
       "/bylaws": { page: "/bylaws" },
       "/donate": { page: "/donate" },
-      // "/friends-of-pride": { page: "/friends-of-pride" },
-      "/tacos-and-tattas": { page: "/tacos-and-tattas" },
       "/our-team": { page: "/our-team" },
       "/serve-on-the-board": { page: "/serve-on-the-board" },
       "/store": { page: "/store" },
       "/volunteer": { page: "/volunteer" },
-      "/parade-registration": { page: "/parade-registration" },
-      // "/pageant-registration": { page: "/pageant-registration" },
-      "/pageant": { page: "/events/pageant" },
       "/vendor-registration": { page: "/vendor-registration" },
       "/gold-membership": { page: "/gold-membership" },
-      "/kaleidoscope": { page: "events/kaleidoscope" },
     };
 
     return paths;
