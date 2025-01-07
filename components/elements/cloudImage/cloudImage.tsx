f3f50d2d92c794cc96d58a2a929a94889f4da9f5
Pete Bingel <peter.bingel@charlestonpride.org>
2025-01-07 13:57:57 -0500

    Fix odd issue with image

--- components/elements/cloudImage/cloudImage.tsx
+++ components/elements/cloudImage/cloudImage.tsx
@@ -1,22 +1,16 @@
-
-
 type ImageProps = {
   imageId: string;
   className?: string;
 };
 
-const CloudImage = ({
-  imageId,
-  className,
-}: ImageProps) => {
+const CloudImage = ({ imageId, className }: ImageProps) => {
   const baseUrl = "https://charleston-pride.stream.prepr.io/";
-  if (imageId.split('.').pop() === imageId) {
+
+  if (imageId.split(".").pop() === imageId) {
     imageId += ".jpg";
   }
-  const src = baseUrl + imageId
-  return (
-    <img src={src} className={className ?? 'img-fluid'} />
-  );
+  const src = imageId.startsWith(baseUrl) ? imageId : baseUrl + imageId;
+  return <img src={src} className={className ?? "img-fluid"} />;
 };
 
 export default CloudImage;
